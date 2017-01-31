a4b6bb5b2330f1c69dbd2ee99e994fa8d486daf9
kotapiku <iturtle@iturtle-MacBook-Air.local>
2017-01-31 22:56:06 +0900

    Anniversary

--- kotatsu/anniversary.py
+++ kotatsu/anniversary.py
@@ -1,17 +1,31 @@
+import os
 import datetime
 from slacker import Slacker
 import slackbot_settings
 
+ANV_Y = os.environ.get('ANV_Y', None)
+ANV_M = os.environ.get('ANV_M', None)
+ANV_D = os.environ.get('ANV_D', None)
+
+
+def dif(a, b):
+    y = a[0] - b[0]
+    m = a[1] - b[1]
+    if m < 0:
+        (y, m) = (y - 1, m + 12)
+    return (y, m)
+
 
 def anv():
     today = datetime.date.today()
-    (yr, mt, dy) = (today.year, today.month, today.day)
+    (yr, mt) = dif([today.year, today.month], [ANV_Y, ANV_M])
+    dy = today.day - ANV_D
 
-    if dy == 18:
-        if yr == 2017 and mt == (3 or 6):
+    if dy == 0:
+        if yr == 0 and mt == (3 or 6):
             return "今日は付き合い始めてから{}ヶ月です!".format(mt)
-        elif mt == 12:
-            return "今日は付き合い始めてから{}年です!".format(yr - 2016)
+        elif mt == 0:
+            return "今日は付き合い始めてから{}年です!".format(yr)
 
 
 slack = Slacker(slackbot_settings.API_TOKEN)
--- kotatsu/response.py
+++ kotatsu/response.py
@@ -3,7 +3,7 @@ from slackbot.bot import listen_to
 @listen_to('疲れた')
 @listen_to('つかれた')
 def rps(message):
-    message.send(':amane:おつかれ:blush:')
+    message.send('おつかれ:blush:')
 
 @listen_to("1+1")
 @listen_to("いちたすいち")
