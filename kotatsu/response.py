60eea8d8579cf337b2863f3e0724065e68ec60b9
kotapiku <iturtle@iturtle-MacBook-Air.local>
2017-01-31 23:49:54 +0900

    みそスープ修正

--- kotatsu/response.py
+++ kotatsu/response.py
@@ -4,3 +4,8 @@ from slackbot.bot import listen_to
 @listen_to('つかれた')
 def rps(message):
     message.send('おつかれ:blush:')
+
+@listen_to('1\+1')
+@listen_to('いちたすいち')
+def msp(message):
+    message.send(':miso:みそスープ')
