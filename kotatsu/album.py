23be4c678906febd3e8bed3c53ebdcec94962f89
kotapiku <iturtle@iturtle-MacBook-Air.local>
2017-02-01 12:12:28 +0900

    Avoid taking from sender’s album

--- kotatsu/album.py
+++ kotatsu/album.py
@@ -4,18 +4,24 @@ from apiclient import discovery
 from slackbot.bot import listen_to
 
 from .utils import get_credentials
+import json
 
 
 @listen_to('^(?:しゃしん|写真)$')
 def photo(message):
+    with open('user_config.json') as json_data_file:
+        data = json.load(json_data_file)
+
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
-    
+    send_user = message.channel._client.users[message.body['user']][u'name']
+
     results = service.files().list(spaces='drive', pageSize=1000).execute().get(
         'files', [])
-    photos = [res['id'] for res in results if '.jpg' in res['name']]
+    photos = [res['id'] for res in results if ('.jpg' in res['name']) and (res['id'][2] != data[send_user])]
     photo_id = np.random.choice(photos)
+
     url = 'http://drive.google.com/uc?export=view&id={}'.format(photo_id)
-    
+
     message.send(url)
