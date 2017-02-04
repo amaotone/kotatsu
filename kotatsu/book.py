156a8350ff456f1c8be3ca59d73e49a68ddc9756
Amane Suzuki <amane.suzu@gmail.com>
2017-02-05 00:34:00 +0900

    Add 503 error handler and ResultNotFoundError handler

--- kotatsu/book.py
+++ kotatsu/book.py
@@ -1,9 +1,19 @@
 import json
 import os
+import random
+import time
+from urllib.error import HTTPError
 
 from bottlenose import api
 from bs4 import BeautifulSoup
 from slackbot.bot import listen_to
+
+
+def error_handler(err):
+    ex = err['exception']
+    if isinstance(ex, HTTPError) and ex.code == 503:
+        time.sleep(random.expovariate(0.1))
+        return True
 
 
 def fetch_book_data(word):
@@ -11,11 +21,14 @@ def fetch_book_data(word):
     secret = os.environ['AMAZON_SECRET']
     tag = os.environ['AMAZON_ASSOCIATE_TAG']
     
-    # TODO: 例外処理してないからよわい
-    amazon = api.Amazon(key, secret, tag, Region='JP')
+    amazon = api.Amazon(key, secret, tag, Region='JP',
+                        ErrorHandler=error_handler)
     res = amazon.ItemSearch(Keywords=word, SearchIndex='Books',
                             ResponseGroup='Medium')
     res = BeautifulSoup(res, 'lxml')
+    
+    if res.totalresults.text == '0':
+        raise RuntimeError('Search resullts could not be found.')
     
     return [{
         'title': res.title.text,
@@ -27,4 +40,7 @@ def fetch_book_data(word):
 
 @listen_to('^(?:本|ほん|book)[\s　]+(.*)')
 def book(message, word):
-    message.send_webapi('', json.dumps(fetch_book_data(word)))
+    try:
+        message.send_webapi('', json.dumps(fetch_book_data(word)))
+    except RuntimeError:
+        message.send('検索結果が見つかりませんでした :sweat_drops:')
