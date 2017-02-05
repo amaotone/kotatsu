8f1d9375c69bb4406ecd66003f18d127cbc27cf2
Amane Suzuki <amaotone@users.noreply.github.com>
2017-02-05 15:51:46 +0900

    Update album.py

--- kotatsu/album.py
+++ kotatsu/album.py
@@ -10,7 +10,7 @@ from slackbot.bot import listen_to
 from .utils import get_credentials
 
 
-@listen_to(r'^(?:しゃしん|写真)[\s　]*(\d*)?')
+@listen_to(r'^(?:しゃしん|写真|photo)[\s　]*(\d*)?')
 def photo(message, number):
     if not number:
         number = 1
@@ -20,18 +20,28 @@ def photo(message, number):
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
-    send_user = message.channel._client.users[message.body['user']]['name']
+    user_name = message.channel._client.users[message.body['user']]['name']
+    user_album = json.loads(os.environ['USER_ALBUM'])[user_name]
     
-    user_album = json.loads(os.environ['USER_ALBUM'])
+    files = []
+    page_token = None
+    while True:
+        response = service.files().list(
+            spaces='drive',
+            q="mimeType='image/jpeg' and not ('{}' in parents)".format(
+                user_album),
+            fields='nextPageToken, files(id)',
+            pageToken=page_token,
+            pageSize=1000).execute()
+        files += response.get('files', [])
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
     
-    response = service.files().list(spaces='drive', pageSize=1000).execute()
-    files = response.get('files', [])
-    photos = [f['id'] for f in files if
-              ('.jpg' in f['name']) and (f['id'][2] != user_album[send_user])]
-    photo_ids = np.random.choice(photos, size=number)
-    
-    url = 'http://drive.google.com/uc?export=view&id={}'
-    attachments = [{'text': '', 'image_url': url.format(pid)} for pid in
-                   photo_ids]
-    
-    message.send_webapi('', json.dumps(attachments))
+    if files:
+        url = 'http://drive.google.com/uc?export=view&id={}'
+        attachments = [{'text': '', 'image_url': url.format(f['id'])} for f in
+                       np.random.choice(files, size=number)]
+        message.send_webapi('', json.dumps(attachments))
+    else:
+        message.send('写真が見つかりませんでした :sweat_drops:')
