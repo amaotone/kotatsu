f46c6f15b6e26b122a29a6dd39145e3669c7e5db
kotapiku <ice08235523@gmail.com>
2017-09-13 19:59:48 +0900

    no message

--- kotatsu/dialogue.py
+++ kotatsu/dialogue.py
@@ -2,17 +2,14 @@ from slackbot.bot import default_reply
 import requests
 import os
 import json
-context = {}
+
 @default_reply(matchstr='(.*)')
 def dialogue(message, input_text):
-    global context
-    member_id = message.body['user']
     url = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY='+ \
             os.environ.get('DIALOGUE_API_KEY', "")
     headers = {'content-type':'application/json'}
     data = {
             'utt':input_text,
-            'context':context.get(member_id,''),
             'mode':'dialog',
             'place':'東京'
             }
@@ -21,9 +18,7 @@ def dialogue(message, input_text):
             data=json.dumps(data),
             headers=headers
             ).json()
-    context[member_id] = rs['context']
     if rs.get('error'):
         message.reply(rs.get('error')['message'])
     else:
         message.reply(rs.get('utt'))
-
