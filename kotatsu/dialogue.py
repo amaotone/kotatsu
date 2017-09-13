d57658af1bf03cb132dc0ced8d76bd3353901cc9
kotapiku <ice08235523@gmail.com>
2017-09-13 19:48:29 +0900

    コンテキストを記憶

--- kotatsu/dialogue.py
+++ kotatsu/dialogue.py
@@ -2,14 +2,17 @@ from slackbot.bot import default_reply
 import requests
 import os
 import json
-
+context = {}
 @default_reply(matchstr='(.*)')
 def dialogue(message, input_text):
+    global context
+    member_id = message.body['user']
     url = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY='+ \
             os.environ.get('DIALOGUE_API_KEY', "")
     headers = {'content-type':'application/json'}
     data = {
             'utt':input_text,
+            'context':context.get(member_id,''),
             'mode':'dialog',
             'place':'東京'
             }
@@ -18,7 +21,9 @@ def dialogue(message, input_text):
             data=json.dumps(data),
             headers=headers
             ).json()
+    context[member_id] = rs['context']
     if rs.get('error'):
         message.reply(rs.get('error')['message'])
     else:
         message.reply(rs.get('utt'))
+
