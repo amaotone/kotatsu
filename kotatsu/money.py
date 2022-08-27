0601e7af63ae1281a4c3542f842f6f142f0fa749
amaotone <amane.suzu@gmail.com>
2022-08-28 01:17:24 +0900

    feat: comment out deprecated commands

--- kotatsu/money.py
+++ kotatsu/money.py
@@ -1,78 +1,77 @@
-import os
-import psycopg2
-import urllib.parse as urlparse
-from slackbot.bot import listen_to
+# import os
+# import psycopg2
+# import urllib.parse as urlparse
+# from slackbot.bot import listen_to
 
-RATIO = 0.4
+# RATIO = 0.4
 
-def database():
-    data_url = urlparse.urlparse(os.environ['DATABASE_URL'])
-    conn = psycopg2.connect(
-        dbname = data_url.path[1:],
-        user = data_url.username,
-        password = data_url.password,
-        host = data_url.hostname,
-        port = data_url.port
-    )
-    return conn
+# def database():
+#     data_url = urlparse.urlparse(os.environ['DATABASE_URL'])
+#     conn = psycopg2.connect(
+#         dbname = data_url.path[1:],
+#         user = data_url.username,
+#         password = data_url.password,
+#         host = data_url.hostname,
+#         port = data_url.port
+#     )
+#     return conn
 
-def change_val(amount):
-    conn = database()
-    cursor = conn.cursor()
+# def change_val(amount):
+#     conn = database()
+#     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM money")
-    cursor.execute("UPDATE money SET num = " + str(amount))
+#     cursor.execute("SELECT * FROM money")
+#     cursor.execute("UPDATE money SET num = " + str(amount))
 
-    conn.commit()
-    cursor.close()
-    conn.close()
+#     conn.commit()
+#     cursor.close()
+#     conn.close()
 
-    return
+#     return
 
-def fetch_val():
-    conn = database()
-    cursor = conn.cursor()
+# def fetch_val():
+#     conn = database()
+#     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM money")
-    num = cursor.fetchone()[0]
+#     cursor.execute("SELECT * FROM money")
+#     num = cursor.fetchone()[0]
 
-    cursor.close()
-    conn.close()
+#     cursor.close()
+#     conn.close()
 
-    return num
+#     return num
 
-@listen_to(r'^(?:(-?\d+))$')
-def add(message, amount):
-    now = fetch_val()
-    amount = int(amount)
-    if amount<0:
-        amount = int(amount*(1-RATIO)/RATIO)
-    new = now + amount
-    change_val(new)
+# @listen_to(r'^(?:(-?\d+))$')
+# def add(message, amount):
+#     now = fetch_val()
+#     amount = int(amount)
+#     if amount<0:
+#         amount = int(amount*(1-RATIO)/RATIO)
+#     new = now + amount
+#     change_val(new)
 
-    message.send('追加しました: {} -> {}'.format(now, new))
+#     message.send('追加しました: {} -> {}'.format(now, new))
 
-    return
+#     return
 
-@listen_to(r'^(?:reset|リセット)$')
-def reset(message):
-    now = fetch_val()
-    change_val(0)
+# @listen_to(r'^(?:reset|リセット)$')
+# def reset(message):
+#     now = fetch_val()
+#     change_val(0)
 
-    message.send('リセットしました: {} -> 0'.format(now))
+#     message.send('リセットしました: {} -> 0'.format(now))
 
-    return
+#     return
 
-@listen_to(r'^(?:いくら)$')
-def out(message):
-    now = fetch_val()
+# @listen_to(r'^(?:いくら)$')
+# def out(message):
+#     now = fetch_val()
 
-    if 250<=int(now*RATIO)%500:
-        now = int(now*RATIO)-int(now*RATIO)%500+500
-    else:
-        now = int(now*RATIO)-int(now*RATIO)%500
+#     if 250<=int(now*RATIO)%500:
+#         now = int(now*RATIO)-int(now*RATIO)%500+500
+#     else:
+#         now = int(now*RATIO)-int(now*RATIO)%500
 
-    message.send('支払い金額: {}'.format(now))
+#     message.send('支払い金額: {}'.format(now))
 
-    return
-
+#     return
--- kotatsu/utils.py
+++ kotatsu/utils.py
@@ -1,12 +1,12 @@
-import json
-import os
+# import json
+# import os
 
-from oauth2client.service_account import ServiceAccountCredentials as SAC
+# from oauth2client.service_account import ServiceAccountCredentials as SAC
 
 
-def get_credentials():
-    scope = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive',
-             'https://www.googleapis.com/auth/drive.photos.readonly']
-    secret = os.environ['GOOGLE_CLIENT_SECRET']
-    return SAC.from_json_keyfile_dict(json.loads(secret, strict=False), scope)
+# def get_credentials():
+#     scope = ['https://spreadsheets.google.com/feeds',
+#              'https://www.googleapis.com/auth/drive',
+#              'https://www.googleapis.com/auth/drive.photos.readonly']
+#     secret = os.environ['GOOGLE_CLIENT_SECRET']
+#     return SAC.from_json_keyfile_dict(json.loads(secret, strict=False), scope)
